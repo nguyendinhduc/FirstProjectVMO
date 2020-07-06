91c3ebaf66841aa114bb0ee4f6dc6ef9e725170c
ngocdo4299 <47731206+ngocdo4299@users.noreply.github.com>
2020-07-06 16:12:01 +0700

    NgocDo first commit
    Excercise 29

--- src/com/vmodev/first/Main.java
+++ src/com/vmodev/first/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ShapeDrawer shapeDrawer = new ShapeDrawer();
         shapeDrawer.drawSolidTriangle(4);
+        shapeDrawer.drawBalancedBorderedTriangle(10);
     }
 }
--- src/com/vmodev/first/ShapeDrawer.java
+++ src/com/vmodev/first/ShapeDrawer.java
@@ -7,10 +7,26 @@ public class ShapeDrawer {
             for(int j = 0 ; j< h - i ; j++ ){
                 System.out.print(" ");
             }
-            for(int j = 0 ; j< i ; j++){
+            for(int j = 0 ; j<= i ; j++){
                 System.out.print("* ");
             }
             System.out.println("");
+        }
+
+    }
+    public void drawBalancedBorderedTriangle(int h){
+        for (int i = 1; i <= h; i++) {
+
+            for (int j = h; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if ( j == 1 || j == i || i == h)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
         }
     }
 }
