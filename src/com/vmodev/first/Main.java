42240faf28a6c7d0493df32704907b2a0af62036
baoanh2002 <67898364+baoanh2002@users.noreply.github.com>
2020-07-07 14:26:41 +0700

    first commit

--- src/com/vmodev/first/Main.java
+++ src/com/vmodev/first/Main.java
@@ -2,8 +2,9 @@ package com.vmodev.first;
 
 public class Main {
     public static void main(String[] args) {
-        ShapeDrawer shapeDrawer = new ShapeDrawer();
-        shapeDrawer.drawSolidTriangle(4);
-        shapeDrawer.drawBalancedBorderedTriangle(10);
+        int a[] = {2, 4, 3, 10 ,16, 17, 8, 9, 10, 2, 5, 6, 2, 4 ,6 ,8, 1};
+        PrimeNumber primeNumber = new PrimeNumber();
+        primeNumber.inputPrime(a);
+
     }
 }
