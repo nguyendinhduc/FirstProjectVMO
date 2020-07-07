f73891c9383520c20d9c908355f8368dc0236aeb
larycoder <lenhuchuhiep99@gmail.com>
2020-07-08 03:09:40 +0700

    LH    : initialize MyString project

---
+++ MyString/src/com/vmodev/hieplnc/Main.java
@@ -0,0 +1,9 @@
+package com.vmodev.hieplnc;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        MyString test = new MyString("HeeH");
+        test.display(test.isSymmetry());
+    }
+}
---
+++ MyString/src/com/vmodev/hieplnc/MyString.java
@@ -0,0 +1,102 @@
+package com.vmodev.hieplnc;
+
+public class MyString {
+    private final String varString;
+
+    public MyString(String varString){
+        this.varString = varString;
+    }
+
+    public int sumASCII(){
+        int len = varString.length();
+        int sum = 0;
+        for(int i = 0; i < len; i++){
+            sum += varString.charAt(i);
+        }
+        return sum;
+    }
+
+    public int sumInt(){
+        int len = varString.length();
+        int sum = 0;
+        int tempDigit;
+        for(int i = 0; i < len; i++){
+            tempDigit = varString.charAt(i) - '0';
+            if(tempDigit <= 9 && tempDigit >= 0){
+                sum += tempDigit;
+            }
+        }
+        return sum;
+    }
+
+    public String sort(){
+        int size = varString.length();
+        char[] tempArray = new char[size];
+        for(int i = 0; i < size; i++){
+            tempArray[i] = varString.charAt(i);
+        }
+        sortArray(tempArray, size);
+        String result = "";
+        for(char i : tempArray){
+            result += i;
+        }
+        return result;
+    }
+
+    public String getUpperCase(){
+        int len = varString.length();
+        String sum = "";
+        for(int i = 0; i < len; i++){
+            if(Character.isUpperCase(varString.charAt(i))){
+                sum += varString.charAt(i);
+            }
+        }
+        return sum;
+    }
+
+    public String isSymmetry(){
+        String revert = "";
+        int len = varString.length();
+        for(int i = len - 1; i >= 0; i--){
+            revert += varString.charAt(i);
+        }
+
+        if(varString.equals(revert)) return new String("Symmetry String");
+        else return new String("Not Symmetry String");
+    }
+
+    public String intellijFirstLetter(){
+        int state = 1;
+        int len = varString.length();
+        String tempS = new String(varString);
+        for(int i = 0; i < len; i++){
+//            if(!isSpace(tempS.charAt(i)) && state == 1)
+        }
+    }
+
+    private  static boolean isSpace(char value){
+        return value == ' ' || value == '\t';
+    }
+
+    private static void sortArray(char[] array, int sz){
+        for(int i = 0; i < sz; i++){
+            for(int j = sz - 1; j > i; j--){
+                if(array[j] < array[j - 1]){
+                    char tempChar = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tempChar;
+                }
+            }
+        }
+    }
+
+    public static void display(int value){
+        System.out.println("Display integer:");
+        System.out.println(value);
+    }
+
+    public static void display(String value){
+        System.out.println("Display integer:");
+        System.out.println(value);
+    }
+}
