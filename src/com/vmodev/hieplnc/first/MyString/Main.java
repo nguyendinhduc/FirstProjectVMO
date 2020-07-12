33b843b07b32b9fae2dea6a52bf108d4519906c5
larycoder <lenhuchuhiep99@gmail.com>
2020-07-12 15:35:37 +0700

    LH    : [V0.1] (String advance exercise) implement Levenshtein distance score

--- src/com/vmodev/hieplnc/first/MyString/Main.java
+++ src/com/vmodev/hieplnc/first/MyString/Main.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        MyString test = new MyString("hello viet nam");
-        test.display(test.calculateString("100000000000000000000", "2", "/"));
+//        MyString test = new MyString("hello viet nam");
+//        test.display(test.calculateString("100000000000000000000", "2", "/"));
 
 //        CheckerMath myChecker = new CheckerMath();
 //        System.out.println(myChecker.checkValidMath(")()(())"));
 
-//        SearchString mySearch = new SearchString(new ArrayList<>());
-//        mySearch.addString("Do Thi Minh Ngoc");
-//        mySearch.addString("Do Thi Nhu Ngoc");
-//        mySearch.addString("Do Nhu Minh Ngoc");
-//        mySearch.addString("Le Thi Hoa Ngoc");
-//        mySearch.addString("Do Minh Minh Do");
-//
-//        mySearch.searchString("Minh Ngoc");
+        SearchString mySearch = new SearchString(new ArrayList<>());
+        mySearch.addString("Do Thi Minh Ngoc");
+        mySearch.addString("Do Thi Nhu Ngoc");
+        mySearch.addString("Do Nhu Minh Ngoc");
+        mySearch.addString("Le Thi Hoa Ngoc");
+        mySearch.addString("Do Minh Minh Do");
+
+        mySearch.searchString("Ngoc Hoa");
     }
 }
--- src/com/vmodev/hieplnc/first/MyString/SearchString.java
+++ src/com/vmodev/hieplnc/first/MyString/SearchString.java
@@ -1,6 +1,7 @@
 package com.vmodev.hieplnc.first.MyString;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class SearchString {
@@ -25,6 +26,23 @@ public class SearchString {
 
     private static boolean isSpace(char space){ return space == ' ' || space == '\t'; }
 
+    private int levenshteinDistance(String word, String pattern){
+        if(word.length() == 0 || pattern.length() == 0) return word.length() + pattern.length();
+
+        int[] editDistance = new int[3];
+        editDistance[0] = levenshteinDistance(word.substring(0, word.length() - 1), pattern) + 1;
+        editDistance[1] = levenshteinDistance(word, pattern.substring(0, pattern.length() - 1)) + 1;
+
+        editDistance[2] = levenshteinDistance(word.substring(0, word.length() - 1), pattern.substring(0, pattern.length() - 1));
+        editDistance[2] += word.charAt(word.length() - 1) == pattern.charAt(pattern.length() - 1) ? 0 : 1;
+
+        int minDistance = editDistance[0];
+        for(int i = 0; i < 3; i++){
+            minDistance = Math.min(minDistance, editDistance[i]);
+        }
+        return minDistance;
+    }
+
     private static ArrayList<String> getWord(String word){
         int index = -1;
         ArrayList<String> wordList = new ArrayList<>();
@@ -44,10 +62,54 @@ public class SearchString {
         return wordList;
     }
 
-    public void searchString(String keyWord){
-        ArrayList<String> words = getWord(keyWord);
-        for(String i : words){
-            System.out.println(i);
+    private int sentenceCompare(String sentence, String pattern){
+        ArrayList<String> sentenceWordList = getWord(sentence);
+        ArrayList<String> patternWordList = getWord(pattern);
+
+        int minDistance = -1;
+        int score = 0;
+        for(int i = 0; i < sentenceWordList.size(); i++){
+            for(int j = 0; j < patternWordList.size(); j++){
+                int temp = levenshteinDistance(sentenceWordList.get(i), patternWordList.get(j));
+                if(minDistance > temp || minDistance < 0) minDistance = temp;
+            }
+            score += minDistance;
+            minDistance = -1;
         }
+        return score;
+    }
+
+    private void sortString(List<String> stringList, List<Integer> scoreList){
+        for(int i = 0; i < scoreList.size(); i++){
+            for(int j = i + 1; j < scoreList.size(); j++){
+                int score_1 = scoreList.get(i);
+                int score_2 = scoreList.get(j);
+                if(score_1 > score_2){
+                    String pattern_1 = stringList.get(i);
+                    String pattern_2 = stringList.get(j);
+
+                    scoreList.set(j, score_1);
+                    scoreList.set(i, score_2);
+                    stringList.set(j, pattern_1);
+                    stringList.set(i, pattern_2);
+                }
+            }
+        }
+    }
+
+    private void displayList(List myList){
+        System.out.println("Display List:");
+        for(int i = 0; i < myList.size(); i++){
+            System.out.println(myList.get(i));
+        }
+    }
+
+    public void searchString(String keyWord){
+        List<Integer> scoreList = new ArrayList<>();
+        for(int i = 0; i < listString.size(); i++){
+            scoreList.add(sentenceCompare(keyWord, listString.get(i)));
+        }
+        sortString(listString, scoreList);
+        displayList(listString);
     }
 }
